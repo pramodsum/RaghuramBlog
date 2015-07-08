217cd55cf38e8037bd69acaeac6a392cbc569f2a
Sumedha <pramods@umich.edu>
2015-07-07 22:29:54 -0700

    Adding gitignore

--- node_modules/nodemon/test/fixtures/app.js
+++
@@ -1,2 +0,0 @@
-console.log('starting up @ ' + (process.env.PORT || 8000));
-require('http').createServer(function (req, res) { console.log('Request in'); res.end('ok'); }).listen(process.env.PORT || 8000);
--- node_modules/nodemon/test/fixtures/sigint.js
+++
@@ -1,7 +0,0 @@
-process.on('SIGINT', function() {
-  // do nothing here
-});
-// timer, to keep process running
-setInterval(function() {
-  // console.log('working');
-}, 1000);
